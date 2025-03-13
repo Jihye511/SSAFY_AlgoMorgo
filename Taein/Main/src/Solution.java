3e65e31397423dc96dd952a1ff217d84818065a7
Intae0099 <dlsxo14700@gmail.com>
2025-03-13 15:11:39 +0900

    사용자 설정 테스트

--- Taein/Main/src/Solution.java
+++ Taein/Main/src/Solution.java
@@ -1,9 +1,18 @@
-
+import java.util.*;
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		String[] phone_book = {"119", "97674223", "1195524421"};
+		Arrays.sort(phone_book);
+		System.out.println(Arrays.toString(phone_book));
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = i+1; j < phone_book.length; j++){
+                if(phone_book[j].startsWith(phone_book[i])) {
+                    System.out.println(phone_book[j] + " " + phone_book[i]);
+                }
+            }
+        }
+        
+        
+    }
 }
